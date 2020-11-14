e920fb55cd80d50f290447ead3e58cdaafb0d4de
Damien Deturck <deturckdamien@gmail.com>
2020-11-14 20:09:19 +0100

    ajout bootstrap

--- vue/test.php
+++ vue/test.php
@@ -3,6 +3,8 @@ namespace App\vue;
 class test{
     public function __construct()
     {
-        echo "hello test";
+        $title = "salut";
+        $content = "<p>oui</p>";
+        require "template.php";
     }
 }
